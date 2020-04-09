68a1a271e840ee8cf73fdaed3a7c665085a63234
smmhossain <samimhossain825@gmail.com>
2020-04-09 23:03:06 +0600

    FindEvenOdd class modified

--- FindEvenOdd.py
+++ FindEvenOdd.py
@@ -2,5 +2,7 @@ a = 12;
 
 if(a%2==0):
     print("a is Even number")
+    print("Hello")
+    #python e if-else condition use korar somoy {} use kora hoy na per line er age tab use kora hoy . tab use na korle syntax error dekhabe
 else:
     print("Odd number")
