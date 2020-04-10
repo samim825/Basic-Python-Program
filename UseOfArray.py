d4a6c2f6b6270e8cfa5868ed95ee95a3675ce44f
smmhossain <samimhossain825@gmail.com>
2020-04-10 15:33:44 +0600

    Use of array file upload

---
+++ UseOfArray.py
@@ -0,0 +1,16 @@
+array = [5,10,12,8,15]
+print(array)
+
+countElement = array.count(5)
+print(countElement)
+
+students = ['Rahim','Fahim','Rahima','Karim','Monir']
+print(students)
+
+print(students[1]) #print with calling index
+
+students.append('Samim') #To add new element in array
+print(students)
+print(len(students))
+
+
