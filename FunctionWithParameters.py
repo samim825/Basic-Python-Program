ff588df6140f6d50454b5b8dcea0680af6ab03f9
smmhossain <samimhossain825@gmail.com>
2020-04-10 16:20:35 +0600

    Use of Function in Python

---
+++ FunctionWithParameters.py
@@ -0,0 +1,21 @@
+def sum(num1,num2):
+    a=num1
+    b=num2;
+    return (a+b)
+def sub(num1,num2):
+    a=num1
+    b=num2;
+    return (a-b)
+def mul(num1,num2):
+    a=num1
+    b=num2;
+    return (a*b)
+def div(num1,num2):
+    a=num1
+    b=num2;
+    return (a/b)
+
+print(sum(5,10))
+print(sub(10,5))
+print(mul(2,4))
+print(div(12,4))
---
+++ SimpleFunction.py
@@ -0,0 +1,6 @@
+def simpleFunction():
+    print("This is a function")
+    a,b = 5,10
+    print(a+b)
+
+simpleFunction();
