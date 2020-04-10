10e8bc7c9823134da4c55faa8b633ab7da38867d
smmhossain <samimhossain825@gmail.com>
2020-04-10 15:57:00 +0600

    print an Array Using Loop

---
+++ PrintArrayUsingForLoop.py
@@ -0,0 +1,9 @@
+students = ['Nirjhor','Alal','Misty','Ruhan','Aysa']
+
+for name in students:
+    print(name)
+
+for name in students:
+    if(name=='Misty'):
+        continue
+    print(name)
---
+++ PrintArrayUsingWhileLoop.py
@@ -0,0 +1,6 @@
+students = ['Nirjhor','Alal','Misty','Ruhan','Aysa']
+
+i=0
+while i<=(len(students)-1):
+    print(students[i])
+    i+=1
